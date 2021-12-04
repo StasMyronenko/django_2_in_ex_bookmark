9d84ad8e6df543bbf4471fbe25d843147dbbb4fa
StasMyronenko <stasmyronenko96@gmail.com>
2021-12-04 21:40:52 +0200

    Chapter 6 and fix mistakes

--- account/urls.py
+++ account/urls.py
@@ -34,6 +34,13 @@ urlpatterns = [
 
     path('register/', views.register, name='register'),
 
-    path('edit/', views.edit, name="edit")
+    path('edit/', views.edit, name="edit"),
+
+    path('users/', views.user_list, name='user_list'),
+    # for AJAX request to follow
+    path('users/follow/', views.user_follow, name='user_follow'),
+
+    path('users/<username>/', views.user_detail, name='user_detail'),
+
 
 ]
--- images/views.py
+++ images/views.py
@@ -12,6 +12,14 @@ from common.decorators import ajax_required
 from django.http import HttpResponse
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
+from actions.utils import create_action
+# redis
+import redis
+from django.conf import settings
+
+# Подключение к Redis.
+r = redis.StrictRedis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=settings.REDIS_DB)
+
 
 @login_required
 def image_create(request):
@@ -27,10 +35,9 @@ def image_create(request):
 
             new_item = form.save(commit=False)
 
-
-
             new_item.user = request.user
             new_item.save()
+            create_action(request.user, 'bookmarked image', new_item)
             messages.success(request, 'Image added successfully')
 
             return redirect(new_item.get_absolute_url())
@@ -42,8 +49,12 @@ def image_create(request):
 
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
+    # Увеличиваем количество просмотров картинки на 1.
+    total_views = r.incr('image:{}:views'.format(image.id))
+    # Увеличиваем рейтинг картинки на 1.
+    r.zincrby('image_ranking', image.id, 1)
     return render(request, 'images/image/detail.html',
-                  {'section': 'images', 'image': image})
+                  {'section': 'images', 'image': image, 'total_views': total_views})
 
 
 @ajax_required
@@ -51,12 +62,14 @@ def image_detail(request, id, slug):
 @require_POST
 def image_like(request):
     image_id = request.POST.get('id')
+
     action = request.POST.get('action')
     if image_id and action:
         try:
             image = Image.objects.get(id=image_id)
             if action == 'like':
                 image.users_like.add(request.user)
+                create_action(request.user, 'likes', image)
             else:
                 image.users_like.remove(request.user)
             return JsonResponse({'status': 'ok'})
@@ -85,3 +98,15 @@ def image_list(request):
     if request.is_ajax():
         return render(request, 'images/image/list_ajax.html', {'section': 'images', 'images': images})
     return render(request, 'images/image/list.html', {'section': 'images', 'images': images})
+
+
+@login_required
+def image_ranking(request):
+    # Получаем набор рейтинга картинок
+    image_ranking = r.zrange('image_ranking', 0,  -1, desc=True)[:10]
+    image_ranking_ids = [int(id) for id in image_ranking]
+    print(image_ranking)
+    # Получаем отсортированный список самых популярных картинок.
+    most_viewed = list(Image.objects.filter(id__in=image_ranking_ids))
+    most_viewed.sort(key=lambda x: image_ranking_ids.index(x.id))
+    return render(request, 'images/image/ranking.html', {'section': 'images', 'most_viewed': most_viewed})
